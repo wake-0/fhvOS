98dbc82d815e70d5024b0b0ff4d99c858632ca83
blackjack92 <blackjack92@github.com>
2015-03-14 09:10:32 +0100

    Bug #10 - Started refactoring existing uart code

--- os/src/main.c
+++ os/src/main.c
@@ -4,6 +4,7 @@
 #include "uart_irda_cir.h"
 #include "interrupt.h"
 #include "soc_AM335x.h"
+#include "hal/uart/uart.h"
 
 #define SOC_PRCM_REGS          					 (0x44E00000)
 #define SOC_CM_PER_REGS 	   					 (SOC_PRCM_REGS + 0)
@@ -122,14 +123,7 @@ int main(void) {
 
 	UART0ModuleClkConfig();
 
-	//pin mux
-	/* RXD */
-	HWREG(SOC_CONTROL_REGS + CONTROL_CONF_UART_RXD(0)) =
-			(CONTROL_CONF_UART0_RXD_CONF_UART0_RXD_PUTYPESEL
-					| CONTROL_CONF_UART0_RXD_CONF_UART0_RXD_RXACTIVE);
-	/* TXD */
-	HWREG(SOC_CONTROL_REGS + CONTROL_CONF_UART_TXD(0)) =
-	CONTROL_CONF_UART0_TXD_CONF_UART0_TXD_PUTYPESEL;
+	UARTPinMuxSetup(0);
 
 	//module reset
 	/* Performing Software Reset of the module. */
@@ -283,214 +277,7 @@ void GPIO1ModuleClkConfig(void) {
 		;
 }
 
-void UART0ModuleClkConfig(void) {
-	/* Configuring L3 Interface Clocks. */
 
-	/* Writing to MODULEMODE field of CM_PER_L3_CLKCTRL register. */
-	HWREG(SOC_CM_PER_REGS + CM_PER_L3_CLKCTRL) |=
-	CM_PER_L3_CLKCTRL_MODULEMODE_ENABLE;
-
-	/* Waiting for MODULEMODE field to reflect the written value. */
-	while (CM_PER_L3_CLKCTRL_MODULEMODE_ENABLE
-			!= (HWREG(SOC_CM_PER_REGS + CM_PER_L3_CLKCTRL) &
-			CM_PER_L3_CLKCTRL_MODULEMODE))
-		;
-
-	/* Writing to MODULEMODE field of CM_PER_L3_INSTR_CLKCTRL register. */
-	HWREG(SOC_CM_PER_REGS + CM_PER_L3_INSTR_CLKCTRL) |=
-	CM_PER_L3_INSTR_CLKCTRL_MODULEMODE_ENABLE;
-
-	/* Waiting for MODULEMODE field to reflect the written value. */
-	while (CM_PER_L3_INSTR_CLKCTRL_MODULEMODE_ENABLE
-			!= (HWREG(SOC_CM_PER_REGS + CM_PER_L3_INSTR_CLKCTRL) &
-			CM_PER_L3_INSTR_CLKCTRL_MODULEMODE))
-		;
-
-	/* Writing to CLKTRCTRL field of CM_PER_L3_CLKSTCTRL register. */
-	HWREG(SOC_CM_PER_REGS + CM_PER_L3_CLKSTCTRL) |=
-	CM_PER_L3_CLKSTCTRL_CLKTRCTRL_SW_WKUP;
-
-	/* Waiting for CLKTRCTRL field to reflect the written value. */
-	while (CM_PER_L3_CLKSTCTRL_CLKTRCTRL_SW_WKUP
-			!= (HWREG(SOC_CM_PER_REGS + CM_PER_L3_CLKSTCTRL) &
-			CM_PER_L3_CLKSTCTRL_CLKTRCTRL))
-		;
-
-	/* Writing to CLKTRCTRL field of CM_PER_OCPWP_L3_CLKSTCTRL register. */
-	HWREG(SOC_CM_PER_REGS + CM_PER_OCPWP_L3_CLKSTCTRL) |=
-	CM_PER_OCPWP_L3_CLKSTCTRL_CLKTRCTRL_SW_WKUP;
-
-	/*Waiting for CLKTRCTRL field to reflect the written value. */
-	while (CM_PER_OCPWP_L3_CLKSTCTRL_CLKTRCTRL_SW_WKUP
-			!= (HWREG(SOC_CM_PER_REGS + CM_PER_OCPWP_L3_CLKSTCTRL) &
-			CM_PER_OCPWP_L3_CLKSTCTRL_CLKTRCTRL))
-		;
-
-	/* Writing to CLKTRCTRL field of CM_PER_L3S_CLKSTCTRL register. */
-	HWREG(SOC_CM_PER_REGS + CM_PER_L3S_CLKSTCTRL) |=
-	CM_PER_L3S_CLKSTCTRL_CLKTRCTRL_SW_WKUP;
-
-	/*Waiting for CLKTRCTRL field to reflect the written value. */
-	while (CM_PER_L3S_CLKSTCTRL_CLKTRCTRL_SW_WKUP
-			!= (HWREG(SOC_CM_PER_REGS + CM_PER_L3S_CLKSTCTRL) &
-			CM_PER_L3S_CLKSTCTRL_CLKTRCTRL))
-		;
-
-	/* Checking fields for necessary values.  */
-
-	/* Waiting for IDLEST field in CM_PER_L3_CLKCTRL register to be set to 0x0. */
-	while ((CM_PER_L3_CLKCTRL_IDLEST_FUNC << CM_PER_L3_CLKCTRL_IDLEST_SHIFT)
-			!= (HWREG(SOC_CM_PER_REGS + CM_PER_L3_CLKCTRL) &
-			CM_PER_L3_CLKCTRL_IDLEST))
-		;
-
-	/*
-	 ** Waiting for IDLEST field in CM_PER_L3_INSTR_CLKCTRL register to attain the
-	 ** desired value.
-	 */
-	while ((CM_PER_L3_INSTR_CLKCTRL_IDLEST_FUNC <<
-	CM_PER_L3_INSTR_CLKCTRL_IDLEST_SHIFT)
-			!= (HWREG(SOC_CM_PER_REGS + CM_PER_L3_INSTR_CLKCTRL) &
-			CM_PER_L3_INSTR_CLKCTRL_IDLEST))
-		;
-
-	/*
-	 ** Waiting for CLKACTIVITY_L3_GCLK field in CM_PER_L3_CLKSTCTRL register to
-	 ** attain the desired value.
-	 */
-	while (CM_PER_L3_CLKSTCTRL_CLKACTIVITY_L3_GCLK
-			!= (HWREG(SOC_CM_PER_REGS + CM_PER_L3_CLKSTCTRL) &
-			CM_PER_L3_CLKSTCTRL_CLKACTIVITY_L3_GCLK))
-		;
-
-	/*
-	 ** Waiting for CLKACTIVITY_OCPWP_L3_GCLK field in CM_PER_OCPWP_L3_CLKSTCTRL
-	 ** register to attain the desired value.
-	 */
-	while (CM_PER_OCPWP_L3_CLKSTCTRL_CLKACTIVITY_OCPWP_L3_GCLK
-			!= (HWREG(SOC_CM_PER_REGS + CM_PER_OCPWP_L3_CLKSTCTRL) &
-			CM_PER_OCPWP_L3_CLKSTCTRL_CLKACTIVITY_OCPWP_L3_GCLK))
-		;
-
-	/*
-	 ** Waiting for CLKACTIVITY_L3S_GCLK field in CM_PER_L3S_CLKSTCTRL register
-	 ** to attain the desired value.
-	 */
-	while (CM_PER_L3S_CLKSTCTRL_CLKACTIVITY_L3S_GCLK
-			!= (HWREG(SOC_CM_PER_REGS + CM_PER_L3S_CLKSTCTRL) &
-			CM_PER_L3S_CLKSTCTRL_CLKACTIVITY_L3S_GCLK))
-		;
-
-	/* Configuring registers related to Wake-Up region. */
-
-	/* Writing to MODULEMODE field of CM_WKUP_CONTROL_CLKCTRL register. */
-	HWREG(SOC_CM_WKUP_REGS + CM_WKUP_CONTROL_CLKCTRL) |=
-	CM_WKUP_CONTROL_CLKCTRL_MODULEMODE_ENABLE;
-
-	/* Waiting for MODULEMODE field to reflect the written value. */
-	while (CM_WKUP_CONTROL_CLKCTRL_MODULEMODE_ENABLE
-			!= (HWREG(SOC_CM_WKUP_REGS + CM_WKUP_CONTROL_CLKCTRL) &
-			CM_WKUP_CONTROL_CLKCTRL_MODULEMODE))
-		;
-
-	/* Writing to CLKTRCTRL field of CM_PER_L3S_CLKSTCTRL register. */
-	HWREG(SOC_CM_WKUP_REGS + CM_WKUP_CLKSTCTRL) |=
-	CM_WKUP_CLKSTCTRL_CLKTRCTRL_SW_WKUP;
-
-	/*Waiting for CLKTRCTRL field to reflect the written value. */
-	while (CM_WKUP_CLKSTCTRL_CLKTRCTRL_SW_WKUP
-			!= (HWREG(SOC_CM_WKUP_REGS + CM_WKUP_CLKSTCTRL) &
-			CM_WKUP_CLKSTCTRL_CLKTRCTRL))
-		;
-
-	/* Writing to CLKTRCTRL field of CM_L3_AON_CLKSTCTRL register. */
-	HWREG(SOC_CM_WKUP_REGS + CM_WKUP_CM_L3_AON_CLKSTCTRL) |=
-	CM_WKUP_CM_L3_AON_CLKSTCTRL_CLKTRCTRL_SW_WKUP;
-
-	/*Waiting for CLKTRCTRL field to reflect the written value. */
-	while (CM_WKUP_CM_L3_AON_CLKSTCTRL_CLKTRCTRL_SW_WKUP
-			!= (HWREG(SOC_CM_WKUP_REGS + CM_WKUP_CM_L3_AON_CLKSTCTRL) &
-			CM_WKUP_CM_L3_AON_CLKSTCTRL_CLKTRCTRL))
-		;
-
-	/* Writing to MODULEMODE field of CM_WKUP_UART0_CLKCTRL register. */
-	HWREG(SOC_CM_WKUP_REGS + CM_WKUP_UART0_CLKCTRL) |=
-	CM_WKUP_UART0_CLKCTRL_MODULEMODE_ENABLE;
-
-	/* Waiting for MODULEMODE field to reflect the written value. */
-	while (CM_WKUP_UART0_CLKCTRL_MODULEMODE_ENABLE
-			!= (HWREG(SOC_CM_WKUP_REGS + CM_WKUP_UART0_CLKCTRL) &
-			CM_WKUP_UART0_CLKCTRL_MODULEMODE))
-		;
-
-	/* Verifying if the other bits are set to required settings. */
-
-	/*
-	 ** Waiting for IDLEST field in CM_WKUP_CONTROL_CLKCTRL register to attain
-	 ** desired value.
-	 */
-	while ((CM_WKUP_CONTROL_CLKCTRL_IDLEST_FUNC <<
-	CM_WKUP_CONTROL_CLKCTRL_IDLEST_SHIFT)
-			!= (HWREG(SOC_CM_WKUP_REGS + CM_WKUP_CONTROL_CLKCTRL) &
-			CM_WKUP_CONTROL_CLKCTRL_IDLEST))
-		;
-
-	/*
-	 ** Waiting for CLKACTIVITY_L3_AON_GCLK field in CM_L3_AON_CLKSTCTRL
-	 ** register to attain desired value.
-	 */
-	while (CM_WKUP_CM_L3_AON_CLKSTCTRL_CLKACTIVITY_L3_AON_GCLK
-			!= (HWREG(SOC_CM_WKUP_REGS + CM_WKUP_CM_L3_AON_CLKSTCTRL) &
-			CM_WKUP_CM_L3_AON_CLKSTCTRL_CLKACTIVITY_L3_AON_GCLK))
-		;
-
-	/*
-	 ** Waiting for IDLEST field in CM_WKUP_L4WKUP_CLKCTRL register to attain
-	 ** desired value.
-	 */
-	while ((CM_WKUP_L4WKUP_CLKCTRL_IDLEST_FUNC <<
-	CM_WKUP_L4WKUP_CLKCTRL_IDLEST_SHIFT)
-			!= (HWREG(SOC_CM_WKUP_REGS + CM_WKUP_L4WKUP_CLKCTRL) &
-			CM_WKUP_L4WKUP_CLKCTRL_IDLEST))
-		;
-
-	/*
-	 ** Waiting for CLKACTIVITY_L4_WKUP_GCLK field in CM_WKUP_CLKSTCTRL register
-	 ** to attain desired value.
-	 */
-	while (CM_WKUP_CLKSTCTRL_CLKACTIVITY_L4_WKUP_GCLK
-			!= (HWREG(SOC_CM_WKUP_REGS + CM_WKUP_CLKSTCTRL) &
-			CM_WKUP_CLKSTCTRL_CLKACTIVITY_L4_WKUP_GCLK))
-		;
-
-	/*
-	 ** Waiting for CLKACTIVITY_L4_WKUP_AON_GCLK field in CM_L4_WKUP_AON_CLKSTCTRL
-	 ** register to attain desired value.
-	 */
-	while (CM_WKUP_CM_L4_WKUP_AON_CLKSTCTRL_CLKACTIVITY_L4_WKUP_AON_GCLK
-			!= (HWREG(SOC_CM_WKUP_REGS + CM_WKUP_CM_L4_WKUP_AON_CLKSTCTRL) &
-			CM_WKUP_CM_L4_WKUP_AON_CLKSTCTRL_CLKACTIVITY_L4_WKUP_AON_GCLK))
-		;
-
-	/*
-	 ** Waiting for CLKACTIVITY_UART0_GFCLK field in CM_WKUP_CLKSTCTRL
-	 ** register to attain desired value.
-	 */
-	while (CM_WKUP_CLKSTCTRL_CLKACTIVITY_UART0_GFCLK
-			!= (HWREG(SOC_CM_WKUP_REGS + CM_WKUP_CLKSTCTRL) &
-			CM_WKUP_CLKSTCTRL_CLKACTIVITY_UART0_GFCLK))
-		;
-
-	/*
-	 ** Waiting for IDLEST field in CM_WKUP_UART0_CLKCTRL register to attain
-	 ** desired value.
-	 */
-	while ((CM_WKUP_UART0_CLKCTRL_IDLEST_FUNC <<
-	CM_WKUP_UART0_CLKCTRL_IDLEST_SHIFT)
-			!= (HWREG(SOC_CM_WKUP_REGS + CM_WKUP_UART0_CLKCTRL) &
-			CM_WKUP_UART0_CLKCTRL_IDLEST))
-		;
-}
 
 void UartFIFOConfigure(void) {
 	unsigned int fifoConfig = 0;
