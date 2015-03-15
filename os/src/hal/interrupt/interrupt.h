1fff93ea0cde4cf039047af43b7b7ee1c8612e1a
trylimits <nicohoess@gmail.com>
2015-03-15 09:41:34 +0100

    Added interrupt.h

---
+++ os/src/hal/interrupt/interrupt.h
@@ -0,0 +1,15 @@
+/*
+ * interrupt.h
+ *
+ *  Created on: 15.03.2015
+ *      Author: Vaio
+ */
+
+#ifndef HAL_INTERRUPT_INTERRUPT_H_
+#define HAL_INTERRUPT_INTERRUPT_H_
+
+
+
+
+
+#endif /* HAL_INTERRUPT_INTERRUPT_H_ */
