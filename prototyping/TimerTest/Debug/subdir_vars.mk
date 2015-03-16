4a8882afee3e926818b73c6cb9fae7c9f0f3122f
Marko Petrovic <petrovic-marko@hotmail.com>
2015-03-16 18:19:19 +0100

    added interrupt functions, but not yet tested

---
+++ prototyping/TimerTest/Debug/subdir_vars.mk
@@ -0,0 +1,95 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LDS_SRCS += \
+../AM335x.lds 
+
+S_UPPER_SRCS += \
+../startup_ARMCA8.S 
+
+C_SRCS += \
+../ascii.c \
+../consoleUtils.c \
+../cpu.c \
+../dmtimer.c \
+../interrupt.c \
+../main.c \
+../uart.c \
+../uartConsole.c \
+../uartStdio.c \
+../uart_irda_cir.c 
+
+OBJS += \
+./ascii.o \
+./consoleUtils.o \
+./cpu.o \
+./dmtimer.o \
+./interrupt.o \
+./main.o \
+./startup_ARMCA8.o \
+./uart.o \
+./uartConsole.o \
+./uartStdio.o \
+./uart_irda_cir.o 
+
+S_UPPER_DEPS += \
+./startup_ARMCA8.d 
+
+C_DEPS += \
+./ascii.d \
+./consoleUtils.d \
+./cpu.d \
+./dmtimer.d \
+./interrupt.d \
+./main.d \
+./uart.d \
+./uartConsole.d \
+./uartStdio.d \
+./uart_irda_cir.d 
+
+S_UPPER_DEPS__QUOTED += \
+"startup_ARMCA8.d" 
+
+C_DEPS__QUOTED += \
+"ascii.d" \
+"consoleUtils.d" \
+"cpu.d" \
+"dmtimer.d" \
+"interrupt.d" \
+"main.d" \
+"uart.d" \
+"uartConsole.d" \
+"uartStdio.d" \
+"uart_irda_cir.d" 
+
+OBJS__QUOTED += \
+"ascii.o" \
+"consoleUtils.o" \
+"cpu.o" \
+"dmtimer.o" \
+"interrupt.o" \
+"main.o" \
+"startup_ARMCA8.o" \
+"uart.o" \
+"uartConsole.o" \
+"uartStdio.o" \
+"uart_irda_cir.o" 
+
+C_SRCS__QUOTED += \
+"../ascii.c" \
+"../consoleUtils.c" \
+"../cpu.c" \
+"../dmtimer.c" \
+"../interrupt.c" \
+"../main.c" \
+"../uart.c" \
+"../uartConsole.c" \
+"../uartStdio.c" \
+"../uart_irda_cir.c" 
+
+S_UPPER_SRCS__QUOTED += \
+"../startup_ARMCA8.S" 
+
+
