86e606f0d29d769d661e75de02c935dbbf7261fa
Kevin <kevin.wallis@hotmail.com>
2015-04-01 14:54:59 +0200

    Bug #28 - Added some basic functionality to the scheduler

--- os/src/scheduler/scheduler.c
+++ os/src/scheduler/scheduler.c
@@ -6,5 +6,17 @@
  */
 #include "scheduler.h"
 
+int SchedulerStartProcess(processFunc func) {
+	return SCHEDULER_OK;
+}
+
+int SchedulerRunNextProcess() {
+	return SCHEDULER_OK;
+}
+
+int SchedulerKillProcess(processId_t id) {
+	return SCHEDULER_OK;
+}
+
 
 
--- os/src/scheduler/scheduler.h
+++ os/src/scheduler/scheduler.h
@@ -10,7 +10,39 @@
 
 #include "../platform/platform.h"
 
+/*
+ * Defines for better understanding
+ */
+#define SCHEDULER_OK		0
+#define SCHEDULER_ERROR		-1
+#define PROCESSES_MAX		64
 
+/*
+ * Typedefs
+ */
+typedef uint16_t processId_t;
+typedef void (*processFunc)();
 
+/*
+ * Enums
+ */
+typedef enum {
+	READY,
+	RUNNING,
+	BLOCKED
+} processState_t;
+
+typedef struct {
+	processId_t id;
+	processFunc func;
+	processState_t state;
+} process_t;
+
+/*
+ * Scheduler functions
+ */
+extern int SchedulerStartProcess(processFunc func);
+extern int SchedulerRunNextProcess();
+extern int SchedulerKillProcess(processId_t id);
 
 #endif /* SCHEDULER_SCHEDULER_H_ */
