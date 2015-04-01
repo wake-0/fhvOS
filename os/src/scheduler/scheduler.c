fcdb04c5f35c65e32e48f1275d25f8c518110e72
Kevin <kevin.wallis@hotmail.com>
2015-04-01 14:31:10 +0200

    Bug #28 - Added header and source file for the scheduler

---
+++ os/src/scheduler/scheduler.c
@@ -0,0 +1,10 @@
+/*
+ * scheduler.c
+ *
+ *  Created on: 01.04.2015
+ *      Author: Kevin
+ */
+#include "scheduler.h"
+
+
+
---
+++ os/src/scheduler/scheduler.h
@@ -0,0 +1,16 @@
+/*
+ * scheduler.h
+ *
+ *  Created on: 01.04.2015
+ *      Author: Kevin
+ */
+
+#ifndef SCHEDULER_SCHEDULER_H_
+#define SCHEDULER_SCHEDULER_H_
+
+#include "../platform/platform.h"
+
+
+
+
+#endif /* SCHEDULER_SCHEDULER_H_ */
