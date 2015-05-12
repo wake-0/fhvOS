0e82278371f5f6cface46ea51b436af414a062a1
trylimits <nicohoess@gmail.com>
2015-05-12 21:53:31 +0200

    Bug #29 - Defined interface for ipc manager

---
+++ os/src/ipc/ipcmanager.h
@@ -0,0 +1,24 @@
+/*
+ * ipcmanager.h
+ *
+ *  Created on: 12.05.2015
+ *      Author: Nicolaj Hoess
+ */
+
+#ifndef IPC_IPCMANAGER_H_
+#define IPC_IPCMANAGER_H_
+
+#include "../platform/platform.h"
+
+#define IPC_MANAGER_OK					(1)
+#define IPC_MANAGER_ALREADY_IN_USE		(-1)
+#define IPC_MANAGER_INVALID_NAME		(-2)
+#define IPC_MANAGER_NOT_AUTHORISED		(-3)
+#define IPC_MANAGER_NO_MESSAGE_AVAIL	(-4)
+
+int IpcManagerRegisterNamespace(char* namespace_name);
+int IpcManagerSendMessage(char* namespace_name, char* message);
+int IpcManagerHasMessage(char* namespace_name);
+int IpcManagerGetNextMessage(char* namespace_name, char* message_buffer, int len);
+
+#endif /* IPC_IPCMANAGER_H_ */
