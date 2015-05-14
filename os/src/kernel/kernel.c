86c8e4f8d066a45d2485fe88307ec57ea8832387
Marko Petrovic <petrovic-marko@hotmail.com>
2015-05-14 11:59:09 +0200

    Bug #58 - Implemented KernelGetUptime()

---
+++ os/src/kernel/kernel.c
@@ -0,0 +1,16 @@
+/*
+ * kernel.c
+ *
+ *  Created on: 14.05.2015
+ *      Author: Marko
+ */
+
+#include "kernel.h"
+
+long KernelGetUptime()
+{
+	// TODO Count the start time in millis
+	return 1337;
+}
+
+
---
+++ os/src/kernel/kernel.h
@@ -0,0 +1,13 @@
+/*
+ * kernel.h
+ *
+ *  Created on: 14.05.2015
+ *      Author: Marko
+ */
+
+#ifndef KERNEL_KERNEL_H_
+#define KERNEL_KERNEL_H_
+
+extern long KernelGetUptime();
+
+#endif /* KERNEL_KERNEL_H_ */
