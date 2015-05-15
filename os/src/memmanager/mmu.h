c3b50c435c4282f78429d28af96f1454e5acf9a3
Marko Petrovic <petrovic-marko@hotmail.com>
2015-05-15 22:51:19 +0200

    BUG #23 - mmu documentation extended

--- os/src/memmanager/mmu.h
+++ os/src/memmanager/mmu.h
@@ -64,7 +64,7 @@ typedef struct
 extern int MMUInit(void);
 extern int MMUSwitchToProcess(process_t* process);
 extern int MMUInitProcess(process_t* process);
-extern void MMUHandleDataAbortException();
+extern void MMUHandleDataAbortException(void);
 extern int MMUFreeAllPageFramesOfProcess(process_t* process);
 
 
