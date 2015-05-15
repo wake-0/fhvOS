453d3dd7c2bf03a4570c3b01e29237c985ab7e75
Kevin <kevin.wallis@hotmail.com>
2015-05-15 23:14:27 +0200

    Bug #47 - Added prototype to the project and did some little code refactoring

---
+++ os/src/filesystem/filesystem.h
@@ -0,0 +1,14 @@
+/*
+ * filesystem.h
+ *
+ *  Created on: 15.05.2015
+ *      Author: Kevin
+ */
+
+#ifndef FILESYSTEM_FILESYSTEM_H_
+#define FILESYSTEM_FILESYSTEM_H_
+
+extern int PrintFilesFromRootDirectory(void);
+extern int ReadFile(char* fileSystem, char* fileToRead);
+
+#endif /* FILESYSTEM_FILESYSTEM_H_ */
