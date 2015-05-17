601a0ec9c082cc69bd27cf1727c3bd46ab30bb7f
trylimits <nicohoess@gmail.com>
2015-05-17 16:14:19 +0200

    Bug #58 - Implemented KernelDebug KernelInfo and KernelError

--- os/src/kernel/kernel.c
+++ os/src/kernel/kernel.c
@@ -7,8 +7,11 @@
  */
 
 #include "kernel.h"
+#include "../processmanager/mutex.h"
+#include <stdio.h>
 
 static long uptimeTicks = 0;
+static mutex_t outputMutex;
 
 long KernelGetUptime()
 {
@@ -18,4 +21,50 @@ long KernelGetUptime()
 long KernelTick(int ticks)
 {
 	return (uptimeTicks += ticks);
+}
+
+int	KernelInfo(const char *format, ...)
+{
+	MutexLock(&outputMutex);
+
+	va_list arg;
+	va_start (arg, format);
+	int res = 0;
+	res += printf("[INFO  @%i] ", KernelGetUptime());
+	res += vprintf(format, arg);
+	va_end(arg);
+
+	MutexUnlock(&outputMutex);
+
+	return res;
+}
+int	KernelDebug(const char *format, ...)
+{
+	MutexLock(&outputMutex);
+
+	va_list arg;
+	va_start (arg, format);
+	int res = 0;
+	res += printf("[DEBUG @ %i] ", KernelGetUptime());
+	res += vprintf(format, arg);
+	va_end(arg);
+
+	MutexUnlock(&outputMutex);
+
+	return res;
+}
+int	KernelError(const char *format, ...)
+{
+	MutexLock(&outputMutex);
+
+	va_list arg;
+	va_start (arg, format);
+	int res = 0;
+	res += printf("[ERROR @%i] ", KernelGetUptime());
+	res += vprintf(format, arg);
+	va_end(arg);
+
+	MutexUnlock(&outputMutex);
+
+	return res;
 }
--- os/src/kernel/kernel.h
+++ os/src/kernel/kernel.h
@@ -11,5 +11,8 @@
 
 extern long KernelGetUptime();
 extern long KernelTick(int ticks);
+extern int	KernelInfo(const char *format, ...);
+extern int	KernelDebug(const char *format, ...);
+extern int	KernelError(const char *format, ...);
 
 #endif /* KERNEL_KERNEL_H_ */
