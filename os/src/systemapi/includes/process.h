12129c724b518337d1536637eef522ba9f18c0d7
trylimits <nicohoess@gmail.com>
2015-05-30 11:05:03 +0200

    Bug #31 - Implemented get_process_count()

---
+++ os/src/systemapi/includes/process.h
@@ -0,0 +1,15 @@
+/*
+ * process.h
+ *
+ *  Created on: 30.05.2015
+ *      Author: Nicolaj Hoess
+ */
+
+#ifndef SYSTEMAPI_INCLUDES_PROCESS_H_
+#define SYSTEMAPI_INCLUDES_PROCESS_H_
+
+extern int get_process_count(void);
+
+
+
+#endif /* SYSTEMAPI_INCLUDES_PROCESS_H_ */
