63089b44348c947deeabcadba1f980b788d7c14c
Kevin <kevin.wallis@hotmail.com>
2015-06-05 13:31:08 +0200

    Bug #95 - Added default implementation for the dmx functions.

--- os/src/driver/dmx/driver_dmx.c
+++ os/src/driver/dmx/driver_dmx.c
@@ -7,4 +7,27 @@
 
 #include "driver_dmx.h"
 
+int DMXInit(uint16_t id) {
+	return DRIVER_OK;
+}
+
+int DMXOpen(uint16_t id) {
+	return DRIVER_OK;
+}
+
+int DMXClose(uint16_t id) {
+	return DRIVER_OK;
+}
+
+int DMXWrite(uint16_t id, char* buf, uint16_t len) {
+	return DRIVER_OK;
+}
+
+int DMXRead(uint16_t id, char* buf, uint16_t len) {
+	return DRIVER_OK;
+}
+
+int DMXIoctl(uint16_t id, uint16_t cmd, uint8_t mode, char* buf, uint16_t len) {
+	return DRIVER_OK;
+}
 
