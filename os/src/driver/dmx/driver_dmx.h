6367c771a06617ce9465133367e8611fe6fa513c
Kevin <kevin.wallis@hotmail.com>
2015-06-05 13:29:28 +0200

    Bug #95 - Added function to the dmx header.

--- os/src/driver/dmx/driver_dmx.h
+++ os/src/driver/dmx/driver_dmx.h
@@ -8,8 +8,13 @@
 #ifndef DRIVER_DMX_DRIVER_DMX_H_
 #define DRIVER_DMX_DRIVER_DMX_H_
 
+#include "../driver.h"
 
-
-
+extern int DMXInit	(uint16_t id);
+extern int DMXOpen	(uint16_t id);
+extern int DMXClose	(uint16_t id);
+extern int DMXWrite	(uint16_t id, char* buf, uint16_t len);
+extern int DMXRead	(uint16_t id, char* buf, uint16_t len);
+extern int DMXIoctl	(uint16_t id, uint16_t cmd, uint8_t mode, char* buf, uint16_t len);
 
 #endif /* DRIVER_DMX_DRIVER_DMX_H_ */
