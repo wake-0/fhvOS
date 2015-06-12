fb579d8202e6f3f5f7af9d635eb070d966a88c3f
Kevin <kevin.wallis@hotmail.com>
2015-06-12 11:05:58 +0200

    Bug #95 - Added a test write to the dmx ioctl.

--- os/src/driver/dmx/driver_dmx.c
+++ os/src/driver/dmx/driver_dmx.c
@@ -8,7 +8,11 @@
 #include "driver_dmx.h"
 #include "../../devicemanager/devicemanager.h"
 
+#define DMX_MAX 128 // max. number of DMX data packages.
+
 static device_t uart;
+char DMXBuffer[DMX_MAX];
+
 
 int DMXInit(uint16_t id) {
 	return DRIVER_OK;
@@ -31,8 +35,30 @@ int DMXRead(uint16_t id, char* buf, uint16_t len) {
 }
 
 int DMXIoctl(uint16_t id, uint16_t cmd, uint8_t mode, char* buf, uint16_t len) {
-	// TODO: use the uart which is in the buffer
+	// Get UART from buffer
 	uart = *((device_t*)buf);
+
+	int i;
+
+	// Fill buffer with test data
+	DMXBuffer[0] = 0;
+	for (i = 1; i < DMX_MAX; i++) {
+		DMXBuffer[i] = 170;
+	}
+
+	DeviceManagerInit(uart);
+	DeviceManagerOpen(uart);
+	// Write start byte
+	DeviceManagerWrite(uart, &DMXBuffer[0], 1);
+
+	// Write test data
+	for(i = 1; i < DMX_MAX; i = /*(i + 1) %*/ DMX_MAX) {
+		DeviceManagerWrite(uart, &DMXBuffer[i], sizeof(char));
+
+	}
+
+	// Disable uart
+	DeviceManagerIoctl(uart, 2, 0, NULL, 0);
 
 	return DRIVER_OK;
 }
