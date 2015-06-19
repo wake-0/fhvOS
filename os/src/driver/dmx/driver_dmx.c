6f1ca05360b5e4831c6471d7faa89faef49ebfd1
Kevin <kevin.wallis@hotmail.com>
2015-06-19 07:28:23 +0200

    Bug #95 - Added default code for enabling the gpio for the dmx module

--- os/src/driver/dmx/driver_dmx.c
+++ os/src/driver/dmx/driver_dmx.c
@@ -7,6 +7,7 @@
 
 #include "driver_dmx.h"
 #include "../../devicemanager/devicemanager.h"
+#include "../../hal/gpio/hal_gpio.h"
 
 #define DMX_MAX 128 // max. number of DMX data packages.
 
@@ -15,6 +16,15 @@ char DMXBuffer[DMX_MAX];
 
 
 int DMXInit(uint16_t id) {
+
+	// Enable gpio for the dmx module
+	// TODO: find the right pin
+	/*
+	uint16_t pin = 0;
+	GPIOEnable(pin);
+	GPIOSetPinDirection(pin, PIN_DIRECTION_OUT);
+	GPIOSetPinValue(pin, PIN_VALUE_HIGH);
+	*/
 	return DRIVER_OK;
 }
 
@@ -38,6 +48,8 @@ int DMXIoctl(uint16_t id, uint16_t cmd, uint8_t mode, char* buf, uint16_t len) {
 	// Get UART from buffer
 	uart = *((device_t*)buf);
 
+	// TODO: check uart == nullpointer
+
 	int i;
 
 	// Fill buffer with test data
@@ -52,7 +64,7 @@ int DMXIoctl(uint16_t id, uint16_t cmd, uint8_t mode, char* buf, uint16_t len) {
 	DeviceManagerWrite(uart, &DMXBuffer[0], 1);
 
 	// Write test data
-	for(i = 1; i < DMX_MAX; i = (i + 1) /* % DMX_MAX */) {
+	for(i = 1; i < DMX_MAX; i = (i + 1) % DMX_MAX) {
 		DeviceManagerWrite(uart, &DMXBuffer[i], sizeof(char));
 
 	}
