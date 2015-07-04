1f41ff6521021e188afd6740fe2570497e47966a
Kevin <kevin.wallis@hotmail.com>
2015-07-04 17:40:42 +0200

    Bug #31 - Implemented open_device.

--- api/userimpl/devices.c
+++ api/userimpl/devices.c
@@ -11,23 +11,30 @@
 int open_device(char* device) {
 	systemCallMessage_t message;
 	message.systemCallNumber = SYSTEM_CALL_OPEN_DEVICE;
-	return 0;
+	message.messageArgs.callBuf = device;
+	int handle = 0;
+	message.messageArgs.returnArg = &handle;
+	SystemCall(&message);
+	return *message.messageArgs.returnArg;
 }
 
 int close_device(int handle) {
 	systemCallMessage_t message;
 	message.systemCallNumber = SYSTEM_CALL_CLOSE_DEVICE;
-	return 0;
+	SystemCall(&message);
+	return *message.messageArgs.returnArg;
 }
 
 int ioctl_device(int handle, int mode, int cmd, int len, char* buf) {
 	systemCallMessage_t message;
 	message.systemCallNumber = SYSTEM_CALL_IOCTL_DEVICE;
-	return 0;
+	SystemCall(&message);
+	return *message.messageArgs.returnArg;
 }
 
 int write_device(int handle, int len, char* buf) {
 	systemCallMessage_t message;
 	message.systemCallNumber = SYSTEM_CALL_WRITE_DEVICE;
-	return 0;
+	SystemCall(&message);
+	return *message.messageArgs.returnArg
 }
