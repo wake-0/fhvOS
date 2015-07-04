5ebd5062a81a6e0eaacc8d150f648437ef2c214f
Kevin <kevin.wallis@hotmail.com>
2015-07-04 16:04:10 +0200

    Bug #95 - Refactored dmx driver.

--- os/src/driver/dmx/driver_dmx.c
+++ os/src/driver/dmx/driver_dmx.c
@@ -10,16 +10,12 @@
 #include "../../hal/gpio/hal_gpio.h"
 #include "../../kernel/kernel.h"
 
-// Defines
-#define DMX_MAX 6 // Max. number of DMX data packages.
-#define channel 0
-
 // Static variables
 static device_t uart;
-static char DMXBuffer[DMX_MAX];
 static boolean_t isInitialized;
 static uint8_t pin7 = 7;
 static uint8_t pin17 = 17;
+static device_t uart;
 
 // Static functions
 static void longBreak(device_t* uart);
@@ -38,10 +34,13 @@ int DMXInit(uint16_t id) {
 	GPIOSetPinDirection(pin17, PIN_DIRECTION_OUT);
 	GPIOSetPinValue(pin17, PIN_VALUE_HIGH);
 
-	// Set the default dmx buffer
-	int i = 0;
-	for (i = 0; i < DMX_MAX; i++) {
-		DMXBuffer[i] = 0;
+	// TODO: move this to the high level driver
+	uart = DeviceManagerGetDevice("UART1", 5);
+	// Init uart when necessary
+	if (!isInitialized) {
+		DeviceManagerInit(uart);
+		DeviceManagerOpen(uart);
+		isInitialized = true;
 	}
 
 	return DRIVER_OK;
@@ -64,47 +63,15 @@ int DMXRead(uint16_t id, char* buf, uint16_t len) {
 }
 
 int DMXIoctl(uint16_t id, uint16_t cmd, uint8_t mode, char* buf, uint16_t len) {
-	// Get UART from buffer
-	uart = *((device_t*) buf);
+	if (len > 12) { return DRIVER_ERROR; }
 
-	// Init uart when necessary
-	if (!isInitialized) {
-		DeviceManagerInit(uart);
-		DeviceManagerOpen(uart);
-		isInitialized = true;
-	}
-
-	volatile int i;
-	for (i = 0; i < DMX_MAX; i++) {
-		DMXBuffer[i] = 0;
-	}
-
-	volatile int direction = 0;
-	while (true) {
+	volatile int i = 0;
+	for (i = 0; i < 2; i++) {
+		// start break
 		longBreak(&uart);
 
-		// TODO: move this stuff
-		// sample stuff
-		if (direction == 0) {
-			DMXBuffer[channel] = DMXBuffer[channel] += 5;
-			DMXBuffer[channel+1] = DMXBuffer[channel+1] += 5;
-			if (DMXBuffer[channel] == 200) {
-				direction = 1;
-			}
-		} else if (direction == 1) {
-			DMXBuffer[channel] = DMXBuffer[channel] -= 5;
-			DMXBuffer[channel+1] = DMXBuffer[channel+1] -= 5;
-			if (DMXBuffer[channel] == 0) {
-				direction = 0;
-			}
-		}
-
-		DMXBuffer[channel+3] = 5;
-		DMXBuffer[channel+2] = 5;
-
-
-		for (i = 0; i < DMX_MAX; i++) {
-			DeviceManagerWrite(uart, &DMXBuffer[i], 1);
+		for (i = 0; i < len; i++) {
+			DeviceManagerWrite(uart, &buf[i], sizeof(char));
 
 			// very important otherwise not everything is written by the uart
 			delay(20);
