8433c173d725b102d7a28f4e9c902bbbb9bcbbdb
trylimits <nicohoess@gmail.com>
2015-07-07 08:45:47 +0200

    Bug #106 - Release 1.0.0

--- os/src/kernel/kernel.h
+++ os/src/kernel/kernel.h
@@ -12,7 +12,7 @@
 #include "../scheduler/scheduler.h"
 
 #define KERNEL_VERBOSE_OUTPUT		0
-#define	KERNEL_DEBUG_OUTPUT			1
+#define	KERNEL_DEBUG_OUTPUT			0
 #define KERNEL_ERROR_OUTPUT			1
 #define KERNEL_INFO_OUTPUT			1
 #define KERNEL_DEFAULT_OUTPUT		1
