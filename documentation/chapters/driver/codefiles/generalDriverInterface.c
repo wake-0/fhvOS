75daf2d8af025dc9839a267db3f294a86ac8cd64
trylimits <nicohoess@gmail.com>
2015-07-31 00:54:29 +0200

    Bug #23 - Smaller Fixes

--- documentation/chapters/driver/codefiles/generalDriverInterface.c
+++ documentation/chapters/driver/codefiles/generalDriverInterface.c
@@ -1,8 +1,8 @@
 typedef struct {
-    int (*init)(uint16_t pin);
-    int (*open)(uint16_t pin);
-    int (*close)(uint16_t pin);
-    int (*read)(uint16_t pin, char* buf, uint16_t length);
-    int (*write)(uint16_t pin, char* buf, uint16_t length);
-    int (*ioctl)(uint16_t pin, uint16_t cmd, uint8_t mode, char* buf, uint16_t length);
+    int (*init)(uint16_t payload);
+    int (*open)(uint16_t payload);
+    int (*close)(uint16_t payload);
+    int (*read)(uint16_t payload, char* buf, uint16_t length);
+    int (*write)(uint16_t payload, char* buf, uint16_t length);
+    int (*ioctl)(uint16_t payload, uint16_t cmd, uint8_t mode, char* buf, uint16_t length);
 } driver_t;
