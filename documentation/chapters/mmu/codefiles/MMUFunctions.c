0e7a88900fbb2e45b15907dafb9d69446a9187e5
trylimits <nicohoess@gmail.com>
2015-07-31 12:01:11 +0200

    Bug #23 - Chap 7 Final

--- documentation/chapters/mmu/codefiles/MMUFunctions.c
+++ documentation/chapters/mmu/codefiles/MMUFunctions.c
@@ -1,5 +1,12 @@
+// MMU Functions
 extern int MMUInit(void);
 extern int MMUSwitchToProcess(process_t* process);
 extern int MMUInitProcess(process_t* process);
-extern void MMUHandleDataAbortException(void);
+extern void MMUHandleDataAbortException(context_t* context);
 extern int MMUFreeAllPageFramesOfProcess(process_t* process);
+
+// MemoryManager Functions
+extern int MemoryManagerInit();
+extern pageAddressPointer_t MemoryManagerGetFreePagesInProcessRegion(unsigned int pagesToReserve);
+extern memoryRegionPointer_t MemoryManagerGetRegion(unsigned int memoryRegionNumber);
+extern int MemoryManagerReserveAllDirectMappedRegions(void);
